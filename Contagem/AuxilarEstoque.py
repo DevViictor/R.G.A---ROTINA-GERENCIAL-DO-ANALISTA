5762008362d2b2f185e0853fd0a3afceb0553a76
João Victor Dos Santos Brito <165731025+DevViictor@users.noreply.github.com>
2026-02-24 16:08:44 -0300

    update estoque

--- Contagem/AuxilarEstoque.py
+++ Contagem/AuxilarEstoque.py
@@ -37,7 +37,8 @@ def contagem():
         gridOptions = gb.build()
 
         with col1:
-            if filtro and not newleitor.empty:
+            
+            if not newleitor.empty:
                 utilizacao = newleitor["Utilização livre"].iloc[0]
                 st.metric("Quantidade de produto",utilizacao)
 
